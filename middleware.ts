50f7d7048be931c85ed732da9345b28baf013b28
dijadikan <dikagshhs5@gmail.com>
2026-03-21 12:38:00 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
-export { default } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+})
 
 export const config = { 
-  // Daftarkan folder yang ingin kamu proteksi (hanya admin yang bisa masuk)
   matcher: ["/exry-admin/:path*"] 
 }
