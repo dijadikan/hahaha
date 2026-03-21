3ad83165380fac7b367f02dc45e562784d89b7b4
dijadikan <dikagshhs5@gmail.com>
2026-03-21 12:50:46 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { getServerSession } from "next-auth";
-import SessionProvider from "@/components/SessionProvider"; // Pastikan buat file ini atau hapus jika belum ada
 
 export const metadata: Metadata = {
-  title: "EXRY HUB | SA-MP Modding Community",
-  description: "Koleksi modifikasi visual dan script Lua.",
+  title: "EXRY HUB",
+  description: "SA-MP Modding Community",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession();
-
   return (
     <html lang="en">
       <body className="bg-[#050505] text-white">
