3e6374cfcd0e7390d666546dbb3ca8d57c86a81d
dijadikan <dikagshhs5@gmail.com>
2026-03-21 12:05:48 +0700

    Add files via upload

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    const token = req.nextauth.token;
+    const isAccessingAdmin = req.nextUrl.pathname.startsWith("/exry-admin");
+    
+    if (isAccessingAdmin && token?.role !== "admin") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
+
+export const config = { matcher: ["/exry-admin/:path*"] };
